c542cdee2f4dc7c74e1ac48f76cebe1ab382a3cc
Fin <aueaoangkun@kkumail.com>
2020-01-10 16:13:40 +0700

    livee

--- src/com/company/CoffeeMachine.java
+++ src/com/company/CoffeeMachine.java
@@ -1,10 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     //attribute
     int water = 10;
     int coffee = 10;
     //method
+    public  void  MakeCoffee(int iAmericano , int iLatte){
+        if (iAmericano > 0) {
+            for (int i = 0; i < iAmericano; i++)
+                if ( americano  != null)
+                    System.out.println("Here your americano");
+                else
+                    System.out.println("Sorry can't make it.");
+            }
+        }
+
+    }
     public Americano MakeAmericano() {
 
         if (coffee > 1 && water > 2) {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public  static  void  main(String[] args) {
         CoffeeMachine coffeeMachine = new CoffeeMachine();
-        Americano americano1 = coffeeMachine.MakeAmericano();
-        Americano americano2 = coffeeMachine.MakeAmericano();
+        coffeeMachine.MakeAmericano(iAmerikano : 6);
+
     }
 }
